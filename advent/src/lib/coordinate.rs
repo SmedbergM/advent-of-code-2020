f612ff63dd6e77ab4411dca36ada4733c80a7770
Matthew Smedberg <matthew.smedberg@gmail.com>
2020-11-29 04:35:50 +0000

    Day 0: CI and challenge automation.

---
+++ advent/src/lib/coordinate.rs
@@ -0,0 +1,90 @@
+// Utility for dealing with two-dimensional coordinate systems
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct XY {
+    pub x: usize,
+    pub y: usize
+}
+
+impl XY {
+
+    pub fn new(x: usize, y: usize) -> XY {
+        XY { x, y}
+    }
+
+    pub fn north(&self) -> Option<XY> {
+        match self.y {
+            0 => None,
+            _ => Some(XY { x: self.x, y: self.y - 1 })
+        }
+    }
+
+    pub fn south(&self) -> XY {
+        XY { x: self.x, y: self.y + 1 }
+    }
+
+    pub fn west(&self) -> Option<XY> {
+        match self.x {
+            0 => None,
+            _ => Some(XY { x: self.x - 1, y: self.y })
+        }
+    }
+
+    pub fn east(&self) -> XY {
+        XY { x: self.x + 1, y: self.y }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn north_spec() {
+        let xy: XY = XY { x: 5, y: 7};
+        assert_eq!(xy.north(), Some(XY { x: 5, y: 6}));
+
+        let xy: XY = XY { x: 5, y: 0 };
+        assert_eq!(xy.north(), None);
+
+        let xy: XY = XY { x: 0, y: 7 };
+        assert_eq!(xy.north(), Some( XY { x: 0, y: 6 }));
+    }
+
+    #[test]
+    fn south_spec() {
+        let xy: XY = XY { x: 5, y: 7 };
+        assert_eq!(xy.south(), XY { x: 5, y: 8 });
+
+        let xy: XY = XY { x: 5, y: 0 };
+        assert_eq!(xy.south(), XY { x: 5, y: 1 });
+
+        let xy: XY = XY { x: 0, y: 7 };
+        assert_eq!(xy.south(), XY { x: 0, y: 8 });
+    }
+
+    #[test]
+    fn west_spec() {
+        let xy: XY = XY { x: 5, y: 7};
+        assert_eq!(xy.west(), Some(XY { x: 4, y: 7}));
+
+        let xy: XY = XY { x: 5, y: 0 };
+        assert_eq!(xy.west(), Some(XY { x: 4, y: 0 }));
+
+        let xy: XY = XY { x: 0, y: 7 };
+        assert_eq!(xy.west(), None);
+    }
+
+    #[test]
+    fn east_spec() {
+        let xy: XY = XY { x: 5, y: 7 };
+        assert_eq!(xy.east(), XY { x: 6, y: 7 });
+
+        let xy: XY = XY { x: 5, y: 0 };
+        assert_eq!(xy.east(), XY { x: 6, y: 0 });
+
+        let xy: XY = XY { x: 0, y: 7 };
+        assert_eq!(xy.east(), XY { x: 1, y: 7 });
+    }
+}
+
---
+++ scripts/challenge.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# Run all challenges whose solution code has changed since the last commit.
+
+import argparse
+import subprocess
+import os.path
+import re
+
+def build_solver(day):
+    cmd = ["cargo", "build", "--release", "--bin", day]
+    subprocess.run(cmd, cwd="advent").check_returncode()
+
+def solve_challenge(day, args):
+    challenge_file = "{}/{}/challenge".format(args.challenge, day)
+    solver = "advent/target/release/{}".format(day)
+
+    with open(challenge_file, 'rb') as challenge:
+        subprocess.run(solver, stdin=challenge, text="UTF-8").check_returncode()
+
+def main(args):
+    # First: use git to list all files changed since the last commit.
+    # Using a naive strategy (if the file `src/bin/dayXY.rs` or any file in `src/bin/dayXY/` changes),
+    # re-run any updated solvers.
+
+    standalone_pat = re.compile(r"(day\d+).rs")
+    directory_pat = re.compile(r"advent/src/bin/(day\d+)")
+
+    # We use HEAD^ since this script is intended for merge-protected branches; i.e. the tip of the branch
+    # will always be a merge commit.
+    git_diff_cmd = ["git", "diff", "--name-only", "HEAD^"]
+
+    def extract_day(path):
+        (dirname, basename) = os.path.split(path)
+        if dirname == "advent/src/bin":
+            m = standalone_pat.match(basename)
+            if m:
+                return m.group(1)
+        else:
+            m = directory_pat.match(dirname)
+            if m:
+                return m.group(1)
+
+    git_diff = subprocess.run(git_diff_cmd, capture_output=True, text="UTF-8")
+    git_diff.check_returncode()
+    for path in git_diff.stdout.split('\n'):
+        day = extract_day(path)
+        if day:
+            print("Running solver for {}".format(day))
+            build_solver(day)
+            solve_challenge(day, args)
+
+if "__main__" == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--challenge",
+                        default="/opt/advent-of-code-2020-secret",
+                        help="Root directory of challenge files")
+    args = parser.parse_args()
+    main(args)
